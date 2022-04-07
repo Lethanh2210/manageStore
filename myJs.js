6389f9ae3b8d00561b3e3429bba0a6a122ea431d
LeThanh <kzxlmdtn888@gmail.com>
2022-04-07 22:38:16 +0700

    ..

--- myJs.js
+++ myJs.js
@@ -1,22 +1,45 @@
-let Products = ['Sony Xperia', 'Samsung Galaxy', 'Nokia 6', 'Redmi'];
+let Products = [['Sony Xperia', '5Tr','20/10'],
+    ['Samsung Galaxy', '10Tr','22/10'],
+    ['Nokia 6', '2Tr','10/5']];
 
 // document.getElementById('input').innerHTML += table;
 function Display(){
-    let table = '';
-
+    let table = `<tr>
+    <th>STT</th>
+    <th>Name</th>
+    <th>Price</th>
+    <th>Date Add</th>
+</tr>`;
     for (let i = 0; i < Products.length; i++) {
         table += `<tr>
-        <td>${i}</td>
-        <td>${Products[i]}</td>
+        <td>${i+1}</td>
+        <td>${Products[i][0]}</td>
+        <td>${Products[i][1]}</td>
+        <td>${Products[i][2]}</td>
+        <td><button onclick="delProduct(${i})">Delete</button></td>
+        <td><button onclick="updateProduct(${i})">Update</button></td>
               </tr>`
 
     }
-    document.getElementById('input').innerHTML += table;
+    document.getElementById('input').innerHTML = table;
 }
+Display();
 function addProduct(){
-    let add = prompt('Nhap ten san pham can them:');
-    Products.unshift(add);
+    let Name = prompt('Nhap ten moi');
+    let Price = prompt('Nhap gia');
+    let dateAdd = prompt('Ngay nhap')
+    let str = [Name,Price,dateAdd];
+    Products.unshift(str);
     Display();
 }
-// Display();
+function delProduct(index){
+Products.splice(index,1);
+Display();
+}
+function updateProduct(index){
+    Products[index][0] = prompt('Nhap ten moi',Products[index][0]);
+    Products[index][1] = prompt('Nhap Gia',Products[index][1]);
+    Products[index][2] = prompt('Nhap ',Products[index][2]);
+    Display();
+}
 
